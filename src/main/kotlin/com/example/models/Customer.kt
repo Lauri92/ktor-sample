071211adff609e1ca216093b2e3323835edc751c
Lauri Riikonen <lauri92.riikonen@gmail.com>
2023-07-01 18:32:33 +0300

    Add test file

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -10,4 +10,41 @@ data class Customer(
     val email: String
 )
 
-val customerStorage = mutableListOf<Customer>()
+val customerStorage = mutableListOf(
+    Customer(
+        id = "1",
+        firstName = "Johny",
+        lastName = "Doe",
+        email = "johny.doe@sample.com"
+    ),
+    Customer(
+        id = "2",
+        firstName = "Jane",
+        lastName = "Doe",
+        email = "jane.doe@sample.com"
+    ),
+    Customer(
+        id = "3",
+        firstName = "Anna",
+        lastName = "Malli",
+        email = "anna.malli@sample.com"
+    ),
+    Customer(
+        id = "4",
+        firstName = "Peter",
+        lastName = "Parker",
+        email = "peter.parker@sample.com"
+    ),
+    Customer(
+        id = "5",
+        firstName = "Orgrim",
+        lastName = "Doomhammer",
+        email = "orgrim.doomhammer@sample.com"
+    ),
+    Customer(
+        id = "6",
+        firstName = "Jaina",
+        lastName = "Proudmoore",
+        email = "jaina.proudmoore@sample.com"
+    ),
+)
--- src/main/kotlin/com/example/models/Orders.kt
+++ src/main/kotlin/com/example/models/Orders.kt
@@ -17,7 +17,8 @@ data class OrderItem(
 
 val orderStorage = listOf(
     Order(
-        number = "2020-04-06-01", contents = listOf(
+        number = "2020-04-06-01",
+        contents = listOf(
             OrderItem(item = "Ham Sandwich", amount = 2, price = 5.50),
             OrderItem(item = "Water", amount = 1, price = 1.50),
             OrderItem(item = "Beer", amount = 3, price = 2.30),
@@ -25,7 +26,8 @@ val orderStorage = listOf(
         )
     ),
     Order(
-        number = "2020-04-03-01", contents = listOf(
+        number = "2020-04-03-01",
+        contents = listOf(
             OrderItem(item = "Cheeseburger", amount = 1, price = 8.50),
             OrderItem(item = "Water", amount = 2, price = 1.50),
             OrderItem(item = "Coke", amount = 2, price = 1.76),
